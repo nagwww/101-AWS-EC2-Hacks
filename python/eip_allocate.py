bab39ecab2c420e3853b2e35b10772e1004c8181
Nag <nagwww@gmail.com>
2014-02-22 19:01:17 -0800

    Allocate an EIP

---
+++ python/eip_allocate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+"""
+- Author : Nag m
+- Hack   : Allocate an EIP Address
+- Info   : Allocate an EIP Address
+"""
+
+import boto
+
+def eip():
+    print conn_default
+    add = conn_default.allocate_address()
+    print add
+
+if __name__ == "__main__":
+   conn_default = boto.connect_ec2()
+   eip()
